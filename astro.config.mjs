3faa6f1d39ea22259b4e1be6842b43f399ffe299
edu lenovo <eduardochamacoelguapo@gmail.com>
2024-10-24 10:26:52 +0100

    added npx astro add auth-astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import tailwind from "@astrojs/tailwind";
 
 import db from "@astrojs/db";
 
+import auth from "auth-astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()],
+  integrations: [tailwind(), db(), auth()],
   output: "server",
   adapter: vercel(),
 });
