0e05ef5d6faf1b882c4cc4a437fb312eb5265e98
TheDaidor <danielcastillobalboa@gmail.com>
2024-10-28 11:01:02 +0100

    fix response error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,37 @@
-import { defineMiddleware } from "astro:middleware";
+// src/middleware/auth.ts
 import { getSession } from "auth-astro/server";
 
-const notAuthenticateRoutes = ['/signin', '/signup']
+const notAuthenticateRoutes = ['/signin', '/signup'];
 
+export async function onRequest({ url, locals, redirect, request }, next) {
+    const session = await getSession(request);
+    const isLoggedIn = !!session;
 
-export const onRequest = defineMiddleware(
-    async ({url, locals, redirect, request}, next) => {
-        const session = await getSession(request)
-        const isLoggedIn = !!session // El doble (!!) sirve para que session se convierta en True o False
+    const user = session?.user;
+    
+    // Definir variables en locals
+    locals.isLoggedIn = isLoggedIn;
+    locals.user = null;
+    locals.isAdmin = false;
 
-        const user = session?.user
-
-        
-        locals.isLoggedIn = isLoggedIn
-        locals.user = null
-        locals.isAdmin = false
-
-        if(user) {
-            locals.user = {
-                name: user.name!,
-                email: user.email!,
-            }
-            locals.isAdmin = user.role === 'admin'
-        }
-
-        //Redirects
-        if(isLoggedIn && notAuthenticateRoutes.includes(url.pathname)) {
-            return redirect('/')
-        }
-        
-
-        return next()
+    if (user) {
+        locals.user = {
+            name: user.name,
+            email: user.email,
+        };
+        locals.isAdmin = user.role === 'admin';
     }
-)
+
+    // Si no está autenticado y está tratando de acceder a una ruta protegida
+    if (!isLoggedIn && !notAuthenticateRoutes.includes(url.pathname)) {
+        return redirect('/signin'); // Redirige a la página de inicio de sesión
+    }
+
+    // Redirecciona si el usuario ya está autenticado
+    if (isLoggedIn && notAuthenticateRoutes.includes(url.pathname)) {
+        return redirect('/');
+    }
+    
+    // Pasa al siguiente middleware o manejador de ruta
+    return next();
+}
