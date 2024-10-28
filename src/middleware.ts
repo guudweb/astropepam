7264df24b32ec0e4ffa84d8b9eaaa8450e8b0105
TheDaidor <danielcastillobalboa@gmail.com>
2024-10-28 11:37:34 +0100

    fix signin error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,43 @@
-// src/middleware/auth.ts
+import { defineMiddleware } from "astro:middleware";
 import { getSession } from "auth-astro/server";
 
-const notAuthenticateRoutes = ['/signin', '/signup'];
 
-export async function onRequest({ url, locals, redirect, request }, next) {
-    const session = await getSession(request);
-    const isLoggedIn = !!session;
 
-    const user = session?.user;
-    
-    // Definir variables en locals
-    locals.isLoggedIn = isLoggedIn;
-    locals.user = null;
-    locals.isAdmin = false;
 
-    if (user) {
-        locals.user = {
-            name: user.name,
-            email: user.email,
-        };
-        locals.isAdmin = user.role === 'admin';
+export const onRequest = defineMiddleware(
+    async ({url, locals, redirect, request}, next) => {
+
+        const notAuthenticateRoutes = ['/signin', '/signup']
+        const authenticateRoutes = ['/', '/data', '/program',]
+
+        const session = await getSession(request)
+        const isLoggedIn = !!session // El doble (!!) sirve para que session se convierta en True o False
+
+        const user = session?.user
+
+        
+        locals.isLoggedIn = isLoggedIn
+        locals.user = null
+        locals.isAdmin = false
+
+        if(user) {
+            locals.user = {
+                name: user.name!,
+                email: user.email!,
+            }
+            locals.isAdmin = user.role === 'admin'
+        }
+
+        //Redirects
+        if(isLoggedIn && notAuthenticateRoutes.includes(url.pathname)) {
+            return redirect('/')
+        }
+
+        if(!isLoggedIn && authenticateRoutes.includes(url.pathname)) {
+            return redirect('/signin')
+        }
+        
+
+        return next()
     }
-
-    // Si no está autenticado y está tratando de acceder a una ruta protegida
-    if (!isLoggedIn && !notAuthenticateRoutes.includes(url.pathname)) {
-        return redirect('/signin'); // Redirige a la página de inicio de sesión
-    }
-
-    // Redirecciona si el usuario ya está autenticado
-    if (isLoggedIn && notAuthenticateRoutes.includes(url.pathname)) {
-        return redirect('/');
-    }
-    
-    // Pasa al siguiente middleware o manejador de ruta
-    return next();
-}
+)
