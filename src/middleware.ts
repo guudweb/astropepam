6f3dcea717b6dd946782619ab10d363c3f07c7d1
edu lenovo <eduardochamacoelguapo@gmail.com>
2024-10-29 13:28:48 +0100

    prueba de cambios sin navbar con proteccion

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,48 +1,42 @@
 import { defineMiddleware } from "astro:middleware";
 import { getSession } from "auth-astro/server";
 
-
-
-
 export const onRequest = defineMiddleware(
-    async ({url, locals, redirect, request}, next) => {
+  async ({ url, locals, redirect, request }, next) => {
+    const notAuthenticateRoutes = ["/signin", "/signup"];
+    const authenticateRoutes = ["/", "/data", "/program"];
+    const adminRoutes = ["/program", "/data"];
 
-        const notAuthenticateRoutes = ['/signin', '/signup']
-        const authenticateRoutes = ['/', '/data', '/program',]
-        const adminRoutes = ['/program', '/data']
+    const session = await getSession(request);
+    const isLoggedIn = !!session; // El doble (!!) sirve para que session se convierta en True o False
 
-        const session = await getSession(request)
-        const isLoggedIn = !!session // El doble (!!) sirve para que session se convierta en True o False
+    const user = session?.user;
 
-        const user = session?.user
+    locals.isLoggedIn = isLoggedIn;
+    locals.user = null;
+    locals.isAdmin = false;
 
-        
-        locals.isLoggedIn = isLoggedIn
-        locals.user = null
-        locals.isAdmin = false
-
-        if(user) {
-            locals.user = {
-                name: user.name!,
-                email: user.email!,
-            }
-            locals.isAdmin = user.role === 'admin'
-        }
-
-        //Redirects
-        if(isLoggedIn && notAuthenticateRoutes.includes(url.pathname)) {
-            return redirect('/')
-        }
-
-        if(!isLoggedIn && authenticateRoutes.includes(url.pathname)) {
-            return redirect('/signin')
-        }
-
-        // if(!locals.isAdmin && adminRoutes.includes(url.pathname)) {
-        //     return redirect('/')
-        // }
-        
-
-        return next()
+    if (user) {
+      locals.user = {
+        name: user.name!,
+        email: user.email!,
+      };
+      locals.isAdmin = user.role === "admin";
     }
-)
+
+    //Redirects
+    if (isLoggedIn && notAuthenticateRoutes.includes(url.pathname)) {
+      return redirect("/");
+    }
+
+    if (!isLoggedIn && authenticateRoutes.includes(url.pathname)) {
+      return redirect("/signin");
+    }
+
+    if (!locals.isAdmin && adminRoutes.includes(url.pathname)) {
+      return redirect("/");
+    }
+
+    return next();
+  }
+);
