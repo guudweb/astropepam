6ae6d26304694ca44108eb29842458476f1a07c7
TheDaidor <danielcastillobalboa@gmail.com>
2024-10-31 10:52:20 +0100

    fix navbar & add spinner

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Spinner from './Spinner.astro';
 
 export default function Navbar() {
 
+  const [loading, setLoading] = useState<boolean>(false)
+
   const handleClick = async () => {
+   try{
+    setLoading(true);
     const { signOut } = await import("auth-astro/client");
     await signOut();
     window.location.replace("/signin");
+    setLoading(false);
+   }catch (error) {
+    console.log(error);
+    setLoading(false)
+   }
   }
 
   return (
@@ -13,10 +23,11 @@ export default function Navbar() {
       <a href="/" className="text-xl text-white font-semibold">PPAM - Malabo</a>
       <button
         id="logout"
-        className="border-2 border-white px-5 py-2 rounded-lg text-white text-lg hover:bg-white hover:text-cyan-600 transition-colors"
+        className="border-2 border-white px-5 py-2 rounded-lg text-white text-lg hover:bg-white hover:text-cyan-600 transition-colors disabled:bg-gray-500"
+        disabled={loading}
         onClick={handleClick}
         >
-          Cerrar Sesión
+          {loading ? <Spinner classList='size-6 mx-auto'/> : "Cerrar sesión"}
         </button>
   </div>
   )
