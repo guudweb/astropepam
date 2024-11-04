af96f885447e56dcad87321f18e79d87378ac1c9
TheDaidor <danielcastillobalboa@gmail.com>
2024-11-04 12:17:54 +0100

    add search by name

--- src/components/DataView.tsx
+++ src/components/DataView.tsx
@@ -1,49 +1,23 @@
-import { useEffect, useState } from "react";
-import { type User } from '../interfaces/user.interface'
 import { TableUser } from "./ListUsers/TableUser";
 import { PaginationButton } from "./ListUsers/PaginationButton";
+import useFetchUsers from "../hooks/useFetchUsers";
+import { Search } from "./Search";
+
 
 export const DataView = () => {
-  const [comments, setComments] = useState<User[]>([]);
-  const [limit] = useState(10);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchData = async () => {
-    setIsLoading(true)
-    try {
-      const response = await fetch(`/api/getAllUsers.json?page=${page}&limit=${limit}`);
-      if (!response.ok) {
-        setIsLoading(false)
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-
-      setComments(data.data);
-      setIsLoading(false)
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false)
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [page]);
-
-  const handleNextPage = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
-
-  const handlePreviousPage = () => {
-    setPage((prevPage) => Math.max(prevPage - 1, 1));
-  };
-
+  const limit = 10;
+  const { comments, isLoading, page, setSearchTerm, handleNextPage, handlePreviousPage } = useFetchUsers(limit); // Usa el hook
   return (
     <section className="pb-10">
       <div className="max-w-7xl px-5 lg:px-20 mx-auto mt-8">
+        <Search onChange={(e) => setSearchTerm(e.target.value)}/>
         <div className="overflow-x-auto">
           <TableUser comments={comments} isLoading={isLoading} />
+          {comments.length === 0 && (
+            <p className="text-center text-gray-500 mt-4">
+              No se encontraron resultados.
+            </p>
+          )}
           <footer className="flex items-start gap-2">
             <PaginationButton 
               disabled={page === 1}
