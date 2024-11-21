0d09bd817165ddb6d45c4e814e61d1b7a7bc5225
edu lenovo <eduardochamacoelguapo@gmail.com>
2024-11-21 11:42:30 +0100

    personas interesadas responsive y checkboxes usables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,16 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 
-
 interface User {
-    email: string;
-    name: string;
+  email: string;
+  name: string;
 }
 
-
 declare namespace App {
-    interface Locals {
-        isLoggedIn: boolean;
-        isAdmin: boolean;
-        user: User | null;
-    }
+  interface Locals {
+    isLoggedIn: boolean;
+    isAdmin: boolean;
+    user: User | null;
+  }
 }
